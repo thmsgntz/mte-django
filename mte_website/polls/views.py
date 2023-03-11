11400b3b44c394d71323bd820bfc846cb2aad6c9
Thomas <guntho.neo@gmail.com>
2023-03-11 20:31:46 +0100

    refacto

--- mte_website/polls/views.py
+++ mte_website/polls/views.py
@@ -1,5 +1,5 @@
 import os
-from enum import Enum, auto
+from enum import Enum
 
 from polls.models import Choice, Question
 
@@ -8,15 +8,30 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 
 
-# Create your views here.
-class TemplatePathPolls(Enum):
-    _PATH = 'polls'
-    INDEX = auto()
-    DETAIL = auto()
+class TemplatePolls(Enum):
+    """Enum pour Polls urls."""
 
-    def __str__(self):
-        message = '{0}.html'.format(self.name.lower())
-        return os.path.join(self._PATH.value, message)
+    app_name = 'polls'
+    index = 'index'
+    detail = 'detail'
+    result_s = 'results'
+
+    def to_html(self):
+        """Generate path 'polls/x.html' in templates.
+
+        Used in render: >> render(request,
+        TemplatePolls.index.to_html(), context)
+        """
+        message = '{0}.html'.format(self.value.lower())
+        return os.path.join(self.app_name.value, message)
+
+    def to_reverse(self):
+        """Generate 'polls:x'.
+
+        Usage in reverse:
+        >> HttpResponseRedirect(reverse(TemplatePolls.index.to_reverse(), args=())
+        """
+        return '{0}:{1}'.format(self.app_name.value.lower(), self.value.lower())
 
 
 def index(request):
@@ -26,7 +41,7 @@ def index(request):
     #     "latest_question_list": latest_question_list,
     # }
     # return HttpResponse(template.render(context, request))
-    template = str(TemplatePathPolls.INDEX)
+    template = TemplatePolls.index.to_html()
 
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
@@ -39,14 +54,12 @@ def detail(request, question_id):
     # except Question.DoesNotExist:
     #     raise Http404(f"Question id '{question_id}' does not exist")
     question: Question = get_object_or_404(Question, pk=question_id)
-    template = str(TemplatePathPolls.DETAIL)
-
-    return render(request, template, {'question': question})
+    return render(request, TemplatePolls.detail.to_html(), {'question': question})
 
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+    return render(request, TemplatePolls.result_s.to_html(), {'question': question})
 
 
 def vote(request, question_id):
@@ -60,7 +73,7 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(
-            request, 'polls/detail.html', {
+            request, TemplatePolls.detail.to_html(), {
                 'question': question,
                 'error_message': "You didn't select a choice.",
             },
@@ -73,5 +86,5 @@ def vote(request, question_id):
     # with POST data. This prevents data from being posted twice if a
     # user hits the Back button.
     return HttpResponseRedirect(
-        reverse('polls:results', args=(question.id,)),
+        reverse(TemplatePolls.result_s.to_reverse(), args=(question.id,)),
     )
